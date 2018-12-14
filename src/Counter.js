5d037e4a2c8d59d1c512f07b07f19f05480a2abb
Kirsten Stenquist <kirstenquist5@gmail.com>
2018-12-13 20:55:24 -0700

    initial push

---
+++ src/Counter.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+class Counter extends React.Component {
+  state = { value: 0 }
+
+  inc = () => {
+    this.setState(state => {
+      return { value: state.value + 1 }
+    })
+  }
+  
+  dec = () => {
+    this.setState(state => {
+      return { value: state.value - 1 }
+    })
+  }
+  render() {
+    return(
+      <div style={{ backgroundColor: this.props.name, color: 'white'}}>
+        <p>{this.state.value}</p>
+        <button onClick={this.inc}>+</button>
+        <button onClick={this.dec}>-</button>
+      </div>
+    )
+  }
+}
+
+export default Counter;
